b58e196eeff5b3501f4f1c3abe85680dc531aa75
Yurii Matus <yurii.matus@globallogic>
2020-02-25 10:28:30 +0200

    removed unusable files

---
+++ src/main/java/com/furniturefuture/entity/Product.java
@@ -0,0 +1,23 @@
+package com.furniturefuture.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String productName;
+    private int price;
+    private String productType;
+}
--- src/main/java/com/futurefurniture/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.futurefurniture.repository;
-
-import com.futurefurniture.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-    Role findByName(String name);
-}
